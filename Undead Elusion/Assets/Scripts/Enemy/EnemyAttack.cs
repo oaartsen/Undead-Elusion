6e71646c3cbc759f69222d97c3982311e1306384
oaartsen <32354166+oaartsen@users.noreply.github.com>
2022-05-24 08:05:38 +0200

    Created new level

--- Undead Elusion/Assets/Scripts/Enemy/EnemyAttack.cs
+++ Undead Elusion/Assets/Scripts/Enemy/EnemyAttack.cs
@@ -6,14 +6,21 @@ public class EnemyAttack : MonoBehaviour {
 
     // Configuration parameters
     [SerializeField] float attackDamage = 40f;
+    [SerializeField] float attackRange = 5f;
 
+    // State variables
+    float distanceToTargetPlayer;
 
     // Cached references
     PlayerHealth targetPlayerHealth = null;
+    Transform targetPlayerTransform = null;
 
     // Start is called before the first frame update
     void Start() {
         targetPlayerHealth = FindObjectOfType<PlayerHealth>();
+        if (targetPlayerHealth != null) {
+            targetPlayerTransform = targetPlayerHealth.transform;
+        }
     }
 
     // Update is called once per frame
@@ -24,8 +31,12 @@ public class EnemyAttack : MonoBehaviour {
     public void AttackHitEvent() {
         if (targetPlayerHealth == null) { return; }
 
-        Debug.Log("Zombie Attack");
-        targetPlayerHealth.DamagePlayer(attackDamage);
+        distanceToTargetPlayer = Vector3.Distance(targetPlayerTransform.position, transform.position);
+
+        if (distanceToTargetPlayer < attackRange) {
+            Debug.Log("Zombie Attack");
+            targetPlayerHealth.DamagePlayer(attackDamage);
+        }
 
     }
 
